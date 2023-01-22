e02ef5577a89160c55ab069e2ba7a1efbbce43ad
rawilk <rwilk1@outlook.com>
2023-01-22 16:05:26 +0100

    2023-01-22 Piąty Commit

--- 20230121 - Lab 2/zad 15 - Rafał wilk.py
+++ 20230121 - Lab 2/zad 15 - Rafał wilk.py
@@ -4,7 +4,7 @@ Napisz skrypt który znajduje najczęściej występującą cyfrę w zbiorzę
 
 # Określanie najczęściej występującej wartości w podanym zbiorze danych
 # Lista z częstościa występowania poszczególnych elementów
-digits = [1, 2, 4, 6, 6, 2, 6, 6, 9]
+digits = [1, 2, 1, 5]
 Frequency = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 print(digits)
@@ -12,7 +12,9 @@ print(digits)
 for digit in digits:
     Frequency[digit] += 1
 
+print(Frequency)
 most_common = -1
+
 for i in range(len(Frequency)):
     if Frequency[i] > most_common:
         most_common = i
